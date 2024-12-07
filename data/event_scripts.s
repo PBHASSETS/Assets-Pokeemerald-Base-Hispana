65277188a73abac5bf333f22721e3a2a1e3cb95f
erkey830 <erkey.official@gmail.com>
2024-12-07 01:50:21 +0100

    Todos los exteriores de hoenn listos

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1137,3 +1137,73 @@ EventScript_VsSeekerChargingDone::
 	.include "data/maps/test15/scripts.inc"
 
 	.include "data/maps/test16/scripts.inc"
+
+	.include "data/maps/test17/scripts.inc"
+
+	.include "data/maps/test18/scripts.inc"
+
+	.include "data/maps/test19/scripts.inc"
+
+	.include "data/maps/test20/scripts.inc"
+
+	.include "data/maps/test21/scripts.inc"
+
+	.include "data/maps/test22/scripts.inc"
+
+	.include "data/maps/test23/scripts.inc"
+
+	.include "data/maps/test24/scripts.inc"
+
+	.include "data/maps/test25/scripts.inc"
+
+	.include "data/maps/test26/scripts.inc"
+
+	.include "data/maps/test27/scripts.inc"
+
+	.include "data/maps/test28/scripts.inc"
+
+	.include "data/maps/test29/scripts.inc"
+
+	.include "data/maps/test30/scripts.inc"
+
+	.include "data/maps/test31/scripts.inc"
+
+	.include "data/maps/test32/scripts.inc"
+
+	.include "data/maps/test33/scripts.inc"
+
+	.include "data/maps/test34/scripts.inc"
+
+	.include "data/maps/test35/scripts.inc"
+
+	.include "data/maps/test36/scripts.inc"
+
+	.include "data/maps/test37/scripts.inc"
+
+	.include "data/maps/test38/scripts.inc"
+
+	.include "data/maps/test39/scripts.inc"
+
+	.include "data/maps/test40/scripts.inc"
+
+	.include "data/maps/test41/scripts.inc"
+
+	.include "data/maps/test42/scripts.inc"
+
+	.include "data/maps/test43/scripts.inc"
+
+	.include "data/maps/test44/scripts.inc"
+
+	.include "data/maps/test45/scripts.inc"
+
+	.include "data/maps/test46/scripts.inc"
+
+	.include "data/maps/test47/scripts.inc"
+
+	.include "data/maps/test48/scripts.inc"
+
+	.include "data/maps/test49/scripts.inc"
+
+	.include "data/maps/test50/scripts.inc"
+
+	.include "data/maps/test51/scripts.inc"
